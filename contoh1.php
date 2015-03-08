c96a9d3c405f9a121b09c3c9a926bbbda6eaada2
unknown <andreapaulo@rocketmail.com>
2015-03-09 01:30:30 +0700

    Commit pertama, versi 1

---
+++ contoh1.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Contoh1</title>
+</head>
+<body>  
+<?php
+echo "Hello Friend!!";
+echo "Hello Friend!!";
+echo "Hello Friend!!";
+echo "Hello Friend!!";
+?>
+</body>
+</html>
